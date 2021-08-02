be584a436cc41deda6f1c486bfda48a41ede28d1
Arundhati <arundhatiraicar12@gmail.com>
2021-08-02 22:31:36 +0530

    commit tree e669b643954c719ad880706ed56a35c84b7cfb99

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] 
+  
+  //Copy the above and paste into browser console, then execute by typing in findFive(array) to see 5 is returned
+  
+
+
+  const superbowlWin = record =>{
+      const result = record.find(record =>record.result ==="W" )
+      return result ? result.year :undefined;
+  }
+
+  
